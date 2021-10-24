f3ab9d41d25e7d5bc690a4c74429424b9dd8f651
Alex Hamlin <alex@alexhamlin.co>
2021-10-24 12:41:01 -0700

    Minor tarbuild updates
    Move Close to the end, and use fs.File instead of *os.File since it
    provides the interface we need.

--- internal/tarbuild/tarbuild.go
+++ internal/tarbuild/tarbuild.go
@@ -5,7 +5,7 @@ import (
 	"bytes"
 	"errors"
 	"io"
-	"os"
+	"io/fs"
 	"strings"
 )
 
@@ -30,22 +30,6 @@ type Builder struct {
 // NewBuilder returns a Builder that writes a tar archive to w.
 func NewBuilder(w io.Writer) *Builder {
 	return &Builder{tw: tar.NewWriter(w)}
-}
-
-// Close finishes writing the tar archive if all entries were added
-// successfully, and returns any error encountered while adding entries.
-func (b *Builder) Close() error {
-	if b.err != nil {
-		return b.err
-	}
-
-	b.err = b.tw.Close()
-	if b.err != nil {
-		return b.err
-	}
-
-	b.err = ErrClosed
-	return nil
 }
 
 // AddDirectory adds an entry for a directory with mode 755 at the provided
@@ -83,8 +67,9 @@ func (b *Builder) AddFileContent(path string, content []byte) error {
 
 // AddFile adds an entry for the provided file at the provided path, including
 // the file's mode bits, modification time, and extended attributes, but not
-// including the file's owner, group, or original name.
-func (b *Builder) AddFile(path string, file *os.File) error {
+// including the file's owner, group, or original name. It reads the file to
+// copy it into the archive, but does not close it.
+func (b *Builder) AddFile(path string, file fs.File) error {
 	if b.err != nil {
 		return b.err
 	}
@@ -112,4 +97,20 @@ func (b *Builder) AddFile(path string, file *os.File) error {
 
 	_, b.err = io.Copy(b.tw, file)
 	return b.err
+}
+
+// Close finishes writing the tar archive if all entries were added
+// successfully, and returns any error encountered while adding entries.
+func (b *Builder) Close() error {
+	if b.err != nil {
+		return b.err
+	}
+
+	b.err = b.tw.Close()
+	if b.err != nil {
+		return b.err
+	}
+
+	b.err = ErrClosed
+	return nil
 }
