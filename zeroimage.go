b736357c614bf3751f1fc38d3e837793d58f9967
Alex Hamlin <alex@alexhamlin.co>
2021-10-25 01:03:37 -0700

    Improve log output
    Now you can actually see the progress of the different stages.

--- zeroimage.go
+++ zeroimage.go
@@ -49,7 +49,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	log.SetPrefix("zeroimage error: ")
+	log.SetPrefix("[zeroimage] ")
 	log.SetFlags(0)
 
 	entrypointSourcePath := flag.Arg(0)
@@ -62,51 +62,55 @@ func main() {
 
 	var image *ocibuild.Image
 	if *flagBase == "" {
+		log.Println("Building image from scratch")
 		image = &ocibuild.Image{
 			Config: specsv1.Image{OS: *flagOS, Architecture: *flagArch},
 		}
 	} else {
+		log.Printf("Loading base image: %s", *flagBase)
 		base, err := os.Open(*flagBase)
 		if err != nil {
-			log.Fatal("loading base archive: ", err)
+			log.Fatal("Unable to load base image: ", err)
 		}
 		image, err = ocibuild.LoadArchive(base)
 		if err != nil {
-			log.Fatal("loading base archive: ", err)
+			log.Fatal("Unable to load base image: ", err)
 		}
 		base.Close()
 		if image.Config.OS != *flagOS || image.Config.Architecture != *flagArch {
 			log.Fatalf(
-				"base image platform %s/%s does not match output platform %s/%s",
+				"Base image platform %s/%s does not match output platform %s/%s",
 				image.Config.OS, image.Config.Architecture,
 				*flagOS, *flagArch,
 			)
 		}
 	}
 
+	log.Printf("Adding entrypoint: %s", entrypointTargetPath)
 	entrypoint, err := os.Open(entrypointSourcePath)
 	if err != nil {
-		log.Fatal("reading entrypoint: ", err)
+		log.Fatal("Unable to read entrypoint: ", err)
 	}
 	layer := image.NewLayer()
 	layer.AddDirectory("bin/")
 	layer.AddFile(entrypointTargetPath, entrypoint)
 	if err := layer.Close(); err != nil {
-		log.Fatal("building entrypoint layer: ", err)
+		log.Fatal("Failed to build entrypoint layer: ", err)
 	}
 	entrypoint.Close()
 
 	image.Config.Config.Entrypoint = []string{entrypointTargetPath}
 	image.Config.Config.Cmd = nil
 
+	log.Printf("Writing image: %s", *flagOutput)
 	output, err := os.Create(*flagOutput)
 	if err != nil {
-		log.Fatal("opening output: ", err)
+		log.Fatal("Unable to create output file: ", err)
 	}
 	if err := image.WriteArchive(output); err != nil {
-		log.Fatal("writing image: ", err)
+		log.Fatal("Failed to write image: ", err)
 	}
 	if err := output.Close(); err != nil {
-		log.Fatal("writing image: ", err)
+		log.Fatal("Failed to write image: ", err)
 	}
 }
