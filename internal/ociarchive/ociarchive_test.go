db1c604d927bc70f198dadee916539c41c25f2e9
Alex Hamlin <alex@alexhamlin.co>
2022-03-24 20:03:32 -0700

    Test loading multi-platform OCI archives

--- internal/ociarchive/ociarchive_test.go
+++ internal/ociarchive/ociarchive_test.go
@@ -18,18 +18,7 @@ func TestRoundTripExistingArchive(t *testing.T) {
 	// image pulled with Skopeo. This ensures that we can load a basic
 	// single-platform image from Skopeo, and that our write implementation is at
 	// least good enough for ourselves to load.
-
-	wd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	archiveFile, err := os.Open(filepath.Join(wd, "testdata", "hello-world-linux-arm64.tar"))
-	if err != nil {
-		panic(err)
-	}
-	defer archiveFile.Close()
-
-	index, err := Load(archiveFile)
+	index, err := loadTestdataArchive("hello-world-linux-arm64.tar")
 	if err != nil {
 		t.Fatalf("failed to load original archive: %v", err)
 	}
@@ -64,4 +53,41 @@ func TestRoundTripExistingArchive(t *testing.T) {
 	if diff != "" {
 		t.Errorf("images not equivalent after round-trip (-want +got):\n%s", diff)
 	}
+}
+
+func TestLoadMultiarchArchive(t *testing.T) {
+	// Ensure that we can load a multi-platform OCI archive of the Docker
+	// "hello-world" image pulled with Skopeo.
+	index, err := loadTestdataArchive("hello-world-multiarch.tar")
+	if err != nil {
+		t.Fatalf("failed to load original archive: %v", err)
+	}
+
+	gotPlatforms := make([]string, len(index))
+	for i, entry := range index {
+		gotPlatforms[i] = image.FormatPlatform(entry.Platform)
+	}
+	wantPlatforms := []string{
+		"linux/amd64", "linux/arm/v5", "linux/arm/v7", "linux/arm64/v8",
+		"linux/386", "linux/mips64le", "linux/ppc64le", "linux/riscv64",
+		"linux/s390x", "windows/amd64", "windows/amd64",
+	}
+	if diff := cmp.Diff(wantPlatforms, gotPlatforms); diff != "" {
+		t.Errorf("unexpected list of platforms (-want +got):\n%s", diff)
+	}
+}
+
+func loadTestdataArchive(name string) (image.Index, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	archiveFile, err := os.Open(filepath.Join(wd, "testdata", name))
+	if err != nil {
+		return nil, err
+	}
+	defer archiveFile.Close()
+
+	return Load(archiveFile)
 }
